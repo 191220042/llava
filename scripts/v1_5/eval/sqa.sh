64e52d31359ac632cd797e072aead786e8261702
jzx <1214069454@qq.com>
2024-06-14 09:02:08 +0000

    add w and act

--- scripts/v1_5/eval/sqa.sh
+++ scripts/v1_5/eval/sqa.sh
@@ -4,13 +4,13 @@ python -m llava.eval.model_vqa_science \
     --model-path liuhaotian/llava-v1.5-7b \
     --question-file ./playground/data/eval/scienceqa/llava_test_CQM-A.json \
     --image-folder ./playground/data/eval/scienceqa/images/test \
-    --answers-file ./playground/data/eval/scienceqa/answers/llava-v1.5-7b-sefp-notrans.jsonl \
+    --answers-file ./playground/data/eval/scienceqa/answers/llava-v1.5-7b-sefp-onlyact.jsonl \
     --single-pred-prompt \
     --temperature 0 \
     --conv-mode vicuna_v1
 
 python llava/eval/eval_science_qa.py \
     --base-dir ./playground/data/eval/scienceqa \
-    --result-file ./playground/data/eval/scienceqa/answers/llava-v1.5-7b-sefp-notrans.jsonl \
-    --output-file ./playground/data/eval/scienceqa/answers/llava-v1.5-7b_output_sefp_notrans.jsonl \
-    --output-result ./playground/data/eval/scienceqa/answers/llava-v1.5-7b_result_sefp_notrans.json
+    --result-file ./playground/data/eval/scienceqa/answers/llava-v1.5-7b-sefp-onlyact.jsonl \
+    --output-file ./playground/data/eval/scienceqa/answers/llava-v1.5-7b_output_sefp_onlyact.jsonl \
+    --output-result ./playground/data/eval/scienceqa/answers/llava-v1.5-7b_result_sefp_onlyact.json
